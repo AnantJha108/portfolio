ebc0e4735063647b014aea7a97eabb2df065e78a
AnantJha108 <anantjhapurnea;2~gmail.com>
2024-03-05 19:33:27 +0530

    work on responsive education section

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -9,7 +9,7 @@ const Education = () => {
             <div class="container px-5 py-4 mx-auto">
                 <div class="flex flex-wrap w-full mb-6 flex-col font-medium items-center text-center">
                     <h1 class="sm:text-3xl text-3xl font-medium title-font mb-2 text-gray-900 dark:text-gray-200">Education</h1>
-                    <p class="lg:w-3/4 w-full text-lg leading-relaxed text-gray-500 dark:text-gray-300">Here are some of my Experience on which I have been working on for the past 3 years.</p>
+                    <p class="lg:w-3/4 w-full text-lg leading-relaxed text-gray-500 dark:text-gray-300">My Education details </p>
                 </div>
                 <div class="container px-5 py-4 mx-auto flex flex-wrap justify-center">
                     <div class="lg:w-3/4 md:w-1/2 md:py-6">
@@ -31,7 +31,7 @@ const Education = () => {
                                     </div>
                                 </div>
                             </div>
-                            <div className="flex relative pb-12 ">
+                            <div className="relative pb-12 hidden lg:block">
                                 <div class="h-full w-20 absolute inset-0 flex items-center justify-center">
                                     <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
                                 </div>
@@ -39,17 +39,17 @@ const Education = () => {
                                     <img src={pu} alt="" />
                                 </div>
                             </div>
-                            <div class="lg:w-2/4 md:w-1/2">
+                            <div class="lg:w-2/4 md:w-1/2 hidden lg:block">
                                 <h2 class="font-medium title-font text-gray-900 mb-1 tracking-wider text-lg mt-2 p-4 dark:text-gray-200">Oct 2021 - Sep 2025</h2>
                             </div>
                         </div>
                         <div className='flex'>
-                            <div class="w-2/4 flex-grow pl-4">
+                            <div class="w-2/4 flex-grow pl-4 hidden lg:block">
                                 <div class="flex-grow pl-4">
                                     <h2 class="font-medium title-font text-gray-900 mb-1 tracking-wider text-lg mt-2 p-4 float-end dark:text-gray-200">Passing Year : 2020</h2>
                                 </div>
                             </div>
-                            <div class="flex relative pb-12">
+                            <div class="relative pb-12 hidden lg:block">
                                 <div class="h-full w-20 absolute inset-0 flex items-center justify-center">
                                     <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
                                 </div>
@@ -93,7 +93,7 @@ const Education = () => {
                                     </div>
                                 </div>
                             </div>
-                            <div class="flex relative pb-12">
+                            <div class="relative pb-12 hidden lg:block">
                                 <div class="h-full w-20 absolute inset-0 flex items-center justify-center">
                                     <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
                                 </div>
@@ -101,7 +101,7 @@ const Education = () => {
                                     <img src={svm} className='rounded-full' alt="" />
                                 </div>
                             </div>
-                            <div class="w-2/4">
+                            <div class="w-2/4 hidden lg:block">
                                 <h2 class="font-medium title-font text-gray-900 mb-1 tracking-wider text-lg mt-2 p-4 dark:text-gray-300">Passing Year : 2018</h2>
                             </div>
                         </div>
