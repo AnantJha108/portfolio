c098f845f328234899bd0e9622d61581b4061818
AnantJha108 <anantjhapurnea;2~gmail.com>
2024-04-04 18:49:48 +0530

    modal work

---
+++ src/Data/projects.jsx
@@ -0,0 +1,29 @@
+import tweet from "../images/tweeter.png"
+import ticTac from "../images/ticTac.png"
+import guessGender from "../images/guessGender.png"
+import ecom from "../images/ecom.png"
+
+const ProjectsData = [{
+        image:tweet,
+        title: "Tweeter Clone",
+        description: "Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm."
+    },
+    {
+        image:ticTac,
+        title: "Tic Tac Toe Game",
+        description: "Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm."
+    },
+    {
+        image:guessGender,
+        title: "Ecommerce Web Applications",
+        description: "Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm."
+    },
+    {
+        image:ecom,
+        title: "Guess Details By Name",
+        description: "Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm."
+    },
+]
+
+
+export default ProjectsData;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,56 +1,46 @@
-import React from 'react'
-import tweet from "../images/tweeter.png"
-import ticTac from "../images/ticTac.png"
-import guessGender from "../images/guessGender.png"
-import ecom from "../images/ecom.png"
+import React, { useState } from 'react'
+import ProjectsData from '../Data/projects'
 
 const Projects = () => {
+    let [modalStatus, setModalStatus] = useState(false)
     return (
         <section id="projects" class="text-gray-600 body-font">
-                <div class="container px-5 py-4 mx-auto">
-                    <div class="flex flex-wrap w-full mb-6 flex-col font-medium items-center text-center">
-                        <h1 class="sm:text-3xl text-3xl font-medium title-font mb-2 text-gray-900 dark:text-gray-200">Projects</h1>
-                        <p class="lg:w-3/4 w-full text-lg leading-relaxed text-gray-500 dark:text-gray-300">Here are some of my skills on which I have been working on for the past 3 years.</p>
-                    </div>
-                    <div class="flex flex-wrap">
-                        <div class="xl:w-1/3 md:w-1/2 p-4">
-                            <div class="border border-gray-200 p-6 rounded-lg">
-                                <div class="w-100 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
-                                    <img src={tweet} alt="" />
-                                </div>
-                                <h2 class="text-lg text-gray-900 font-medium title-font mb-2 dark:text-gray-200">Tweeter Clone</h2>
-                                <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
-                            </div>
-                        </div>
-                        <div class="xl:w-1/3 md:w-1/2 p-4">
-                            <div class="border border-gray-200 p-6 rounded-lg">
-                                <div class="w-100 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
-                                    <img src={ticTac} alt="" />
-                                </div>
-                                <h2 class="text-lg text-gray-900 font-medium title-font mb-2 dark:text-gray-200">Tic Tac Toe Game</h2>
-                                <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
-                            </div>
-                        </div>
-                        <div class="xl:w-1/3 md:w-1/2 p-4">
-                            <div class="border border-gray-200 p-6 rounded-lg">
-                                <div class="w-100 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
-                                    <img src={ecom} alt="" />
-                                </div>
-                                <h2 class="text-lg text-gray-900 font-medium title-font mb-2 dark:text-gray-200">Ecommerce Web Applications</h2>
-                                <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
-                            </div>
-                        </div>
-                        <div class="xl:w-1/3 md:w-1/2 p-4">
-                            <div class="border border-gray-200 p-6 rounded-lg">
-                                <div class="w-100 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
-                                    <img src={guessGender} alt="" />
-                                </div>
-                                <h2 class="text-lg text-gray-900 font-medium title-font mb-2 dark:text-gray-200">Guess Details By Name</h2>
-                                <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
-                            </div>
-                        </div>
-                    </div>
+            <div class="container px-5 py-4 mx-auto">
+                <div class="flex flex-wrap w-full mb-6 flex-col font-medium items-center text-center">
+                    <h1 class="sm:text-3xl text-3xl font-medium title-font mb-2 text-gray-900 dark:text-gray-200">Projects</h1>
+                    <p class="lg:w-3/4 w-full text-lg leading-relaxed text-gray-500 dark:text-gray-300">Here are some of my skills on which I have been working on for the past 3 years.</p>
                 </div>
+                <div class="flex flex-wrap">
+                    {ProjectsData.map((projectsItems, i) => {
+                        return (
+                            <>
+                                <div className={`ModalDiv ${modalStatus ? "showModalDiv" : ""}`}>
+                                    <div class="relative p-4 w-full max-w-2xl max-h-full">
+                                        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                            <div class="p-4 md:p-5 space-y-4">
+                                                <div class="w-100 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                                                    <img src={projectsItems.image} alt="" />
+                                                </div>
+                                                <h2 class="text-lg text-gray-900 font-medium title-font mb-2 dark:text-gray-200">{projectsItems.title}</h2>
+                                                <p class="leading-relaxed text-base">{projectsItems.description}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="xl:w-1/3 md:w-1/2 p-4" key={i} onClick={() => setModalStatus(!modalStatus)}>
+                                    <div class="border border-gray-200 p-6 rounded-lg">
+                                        <div class="w-100 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                                            <img src={projectsItems.image} alt="" />
+                                        </div>
+                                        <h2 class="text-lg text-gray-900 font-medium title-font mb-2 dark:text-gray-200">{projectsItems.title}</h2>
+                                        <p class="leading-relaxed text-base">{projectsItems.description}</p>
+                                    </div>
+                                </div>
+                            </>
+                        )
+                    })}
+                </div>
+            </div>
         </section>
     )
 }
