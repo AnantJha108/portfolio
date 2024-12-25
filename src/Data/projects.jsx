c91adbfd0f358614a11115c89c84ddff1355fa42
AnantJha108 <anantjhapurnea;2~gmail.com>
2024-12-25 21:43:12 +0530

    Work on projects descriptions

--- src/Data/projects.jsx
+++ src/Data/projects.jsx
@@ -7,25 +7,25 @@ const ProjectsData = [{
         id:1,
         image:tweet,
         title: "Tweeter Clone",
-        description: "Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm."
+        description: "A Twitter clone using React. Features include posting tweets, likes, comments, user profiles, and real-time updates."
     },
     {
         id:2,
         image:ticTac,
         title: "Tic Tac Toe Game",
-        description: "Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm."
+        description: "Tic Tac Toe is a classic two-player strategy game where players take turns marking X or O on a 3x3 grid, aiming to align three symbols horizontally, vertically, or diagonally."
     },
     {
         id:3,
         image:ecom,
         title: "Ecommerce Web Applications",
-        description: "Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm."
+        description: "E-commerce web applications built with Django leverage robust tools like Django ORM, Django Admin, and REST Framework for secure user authentication, seamless inventory management, payment integration, and scalable deployment.."
     },
     {
         id:4,
         image:guessGender,
         title: "Guess Details By Name",
-        description: "Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm."
+        description: " 'Guess Details By Name' is a project using an API and React as frontend tools. It retrieves and displays user-related data based on names entered in the interface."
     },
 ]
 
